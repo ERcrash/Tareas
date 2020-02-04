ee8ea7e7a4829efb794c20a4ca9f5a91a15fca5d
Edsson Rodriguez <eddy_2020@hotmail.com>
2020-02-03 21:13:37 -0600

    Update de pruea de GIT, una linea nueva a imprimir en pantalla.

---
+++ Act2.js
@@ -0,0 +1,16 @@
+var mes = 10;
+if (mes == 12 || mes <= 2 && mes !== 0 && mes > 0){
+   alert("es invierno");
+}else if(mes < 6 && mes > 2){
+   alert("es primavera");
+}else if(mes < 9 && mes > 5){
+   alert("es verano");
+}else if(mes < 12 && mes > 8){
+   alert("es otoño");  
+  
+}else{
+   alert("No es un mes valido");
+  }
+console.log("Carné: 20000235 Nombre: Edsson Roberto Rodríguez Marroquín" );
+document.write("Carné 20000235 Nombre: Edsson Roberto Rodríguez Marroquín" );
+document.write("Prueba de GIT");
